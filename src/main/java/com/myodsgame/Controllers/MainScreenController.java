758cbf72b3d94255908484e4415b7061f1f7b8d6
nachommarti <ilmarmar@etsinf.upv.es>
2023-04-21 13:12:09 +0200

    Iconos en todas las ventanas y eliminación de muchos warnings

--- src/main/java/com/myodsgame/Controllers/MainScreenController.java
+++ src/main/java/com/myodsgame/Controllers/MainScreenController.java
@@ -1,38 +1,25 @@
 package com.myodsgame.Controllers;
 
-import com.myodsgame.Utils.UserUtils;
-import javafx.application.Platform;
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.BooleanBinding;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Path;
-import java.util.Objects;
 import java.util.Random;
 import java.util.ResourceBundle;
 
 public class MainScreenController implements Initializable {
     @FXML
     private Pane pane;
-    @FXML
-    private Button fastGameButton;
     @FXML
     private Button loginButton;
 
@@ -82,6 +69,7 @@ public class MainScreenController implements Initializable {
         stage.setTitle(challengeTitle);
         stage.initModality(Modality.WINDOW_MODAL);
         stage.setResizable(false);
+        stage.getIcons().add(new Image(Path.of("", "src", "main", "resources", "images", "LogoODS.png").toAbsolutePath().toString()));
         stage.show();
     }
 
@@ -96,6 +84,7 @@ public class MainScreenController implements Initializable {
         stage.setTitle("Login");
         stage.initModality(Modality.WINDOW_MODAL);
         stage.setResizable(false);
+        stage.getIcons().add(new Image(Path.of("", "src", "main", "resources", "images", "LogoODS.png").toAbsolutePath().toString()));
         stage.show();
     }
 
