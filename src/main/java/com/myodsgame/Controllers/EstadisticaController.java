c3e0836ddd991734f398c63b60352157d42bbbe7
nachommarti <ilmarmar@etsinf.upv.es>
2023-05-10 11:30:40 +0200

    Warnings quitados

--- src/main/java/com/myodsgame/Controllers/EstadisticaController.java
+++ src/main/java/com/myodsgame/Controllers/EstadisticaController.java
@@ -1,10 +1,8 @@
 package com.myodsgame.Controllers;
 
 import com.myodsgame.Models.Estadisticas;
-import com.myodsgame.Repository.RepositorioEstadisticas;
 import com.myodsgame.Repository.RepositorioEstadisticasImpl;
 import com.myodsgame.Utils.EstadoJuego;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -14,13 +12,10 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
@@ -33,9 +28,6 @@ public class EstadisticaController implements Initializable {
 
     @FXML
     private Label labelPuntos;
-
-    @FXML
-    private Button botonAtras;
 
     private List<Estadisticas> estadisticas;
 
@@ -98,7 +90,7 @@ public class EstadisticaController implements Initializable {
         ranking.setItems(stats);
     }
     @FXML
-    void botonAtrasPulsado(ActionEvent event) throws IOException {
+    void botonAtrasPulsado(ActionEvent event) {
         ((Stage) ranking.getScene().getWindow()).close();
     }
 
