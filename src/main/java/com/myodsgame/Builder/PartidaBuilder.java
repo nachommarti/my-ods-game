8fbcc4257fff1823294005505e3d940cfba93f67
vict0rsan <vicloligye@hotmail.com>
2023-05-28 23:16:27 +0200

    test partidas builder casi acabado

--- src/main/java/com/myodsgame/Builder/PartidaBuilder.java
+++ src/main/java/com/myodsgame/Builder/PartidaBuilder.java
@@ -9,6 +9,12 @@ import java.nio.file.Path;
 
 public abstract class PartidaBuilder implements IPartidaBuilder{
     protected Partida partida;
+    public static String SONIDO_AYUDA = "src/main/resources/sounds/pista_larga.mp3";
+    public static String SONIDO_ACIERTO = "src/main/resources/sounds/pista_larga.mp3";
+    public static String SONIDO_FALLO = "src/main/resources/sounds/pista_larga.mp3";
+    public static String SONIDO_VICTORIA = "src/main/resources/sounds/pista_larga.mp3";
+    public static String SONIDO_DERROTA = "src/main/resources/sounds/pista_larga.mp3";
+    public static String SONIDO_TICTAC = "src/main/resources/sounds/pista_larga.mp3";
 
     public PartidaBuilder() {
         partida = new Partida();
@@ -16,12 +22,12 @@ public abstract class PartidaBuilder implements IPartidaBuilder{
 
     @Override
     public void BuildSonidos() {
-        partida.setSonidoAyuda(new Media(new File("src/main/resources/sounds/pista_larga.mp3").toURI().toString()));
-        partida.setSonidoAcierto(new Media(new File("src/main/resources/sounds/Acierto.mp3").toURI().toString()));
-        partida.setSondioFallo(new Media(new File("src/main/resources/sounds/Fallo.mp3").toURI().toString()));
-        partida.setSonidoVictoria(new Media(new File("src/main/resources/sounds/Victoria.mp3").toURI().toString()));
-        partida.setSonidoDerrota(new Media(new File("src/main/resources/sounds/Partida_Perdida.mp3").toURI().toString()));
-        partida.setDiezsecs(new Media(new File("src/main/resources/sounds/10S_tick.mp3").toURI().toString()));
+        partida.setSonidoAyuda(new Media(new File(SONIDO_AYUDA).toURI().toString()));
+        partida.setSonidoAcierto(new Media(new File(SONIDO_ACIERTO).toURI().toString()));
+        partida.setSondioFallo(new Media(new File(SONIDO_FALLO).toURI().toString()));
+        partida.setSonidoVictoria(new Media(new File(SONIDO_VICTORIA).toURI().toString()));
+        partida.setSonidoDerrota(new Media(new File(SONIDO_DERROTA).toURI().toString()));
+        partida.setDiezsecs(new Media(new File(SONIDO_TICTAC).toURI().toString()));
     }
 
     @Override
