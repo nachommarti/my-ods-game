2b00ae87d3715a0d1c54b2f8acab10dd69625606
Niko-05 <nicolasmz0511@gmail.com>
2023-06-01 18:54:10 +0200

    hotfix testsData

--- src/main/java/com/myodsgame/Utils/TestData.java
+++ src/main/java/com/myodsgame/Utils/TestData.java
@@ -13,6 +13,5 @@ public class TestData {
         PartidaMixtaBuilder builder = new PartidaMixtaBuilder();
         PartidaDirector director = new PartidaDirector(builder);
         director.BuildPartida();
-        DBConnection db = new DBConnection();
     }
 }
